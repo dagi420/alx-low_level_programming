86ae2ff42984c9be26923cd767b34d58b4807bea
dagi420 <dagiovoxo@gmail.com>
2022-06-30 14:02:31 -0700

    my commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,15 @@
-/*#!/bin/bash*/
 #include <stdio.h>
-
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
 int main(void)
 {
-
-/**
-* main- entry point
-*Return: 0 after printing function
-*/
-
 	puts("\"Programming is like building a multilingual puzzle");
 
 		return (0);
