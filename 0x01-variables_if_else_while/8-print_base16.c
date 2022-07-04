8eb7b76cc885e763fd7f30cae45ce51c050e0ef1
dagi420 <dagiovoxo@gmail.com>
2022-07-04 15:55:32 -0700

    8

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,12 +5,12 @@
  */
 int main(void)
 {
-int i,y;
-for (i=0;i < 10; i++)
+int i, y;
+for ( i=0; i < 10; i++)
 {
 putchar (i + '0');
 }
-for (y= 'a'; y<= 'f'; y++)
+for (y = 'a'; y <= 'f'; y++)
 {
 putchar (y);
 }
