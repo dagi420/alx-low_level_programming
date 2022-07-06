663179d5eee097875cea2a8890eebe8ed36be34e
dagi420 <dagiovoxo@gmail.com>
2022-07-06 12:33:20 -0700

    2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints alphabets 10
- * Description: prints 10 times 
- * Return: Always(0).
- */
+*
+* print_alphabet_x10 - prints alphabets 10
+* Description: prints 10 time
+* Return: Always(0).
+*/
 void print_alphabet_x10(void)
 {
 int alphabet_count = 0;
@@ -14,8 +15,7 @@ while (ch <= 'z')
 {
  _putchar(ch);
 ch++;	
-}
-alphabet_count++;
+}alphabet_count++;
 _putchar('\n');
 }
 }
