7633e1f5430e164d3f49814445594463fc7ef321
dagi420 <dagiovoxo@gmail.com>
2022-07-06 16:45:29 -0700

    103

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-/** 
- * main - main block 
- * Description: print the sum of even Fibonacci nu,bers up to fib value
+/**
+ * main - main block
+ * Description: print the sum of even Fibonacci numbers up to fib value
  * not exceeding 4,000,000.
  * Return: 0
  */
