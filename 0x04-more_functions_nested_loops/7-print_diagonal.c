6ba51f20ef2cb7b34a3e467b9e06b559c6266594
dagi420 <dagiovoxo@gmail.com>
2022-07-08 16:35:17 -0700

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal -draws a diagona; line on the terminal
+ * @n: number of times the character \ should be printed
+ */
+void print_diagonal(void)
+{
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+int i, j;
+for (i = 0; i < n; i++)
+{
+for (j = 0; j < n; j++)
+{
+if (j == 1)
+_putchar('\\');
+else if (j < i)
+_putchar(' ');
+}
+_putchar('\n');
+}
+}
+}
