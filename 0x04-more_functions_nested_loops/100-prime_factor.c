3dd663782bf451b9325114895eb7dac427a46aee
dagi420 <dagiovoxo@gmail.com>
2022-07-08 17:30:27 -0700

    101

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -3,7 +3,7 @@
 /**
  * main - finds nd prints the largest prime factor of the number 612852475143
  * followed by a new line
- * Return: Always0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
