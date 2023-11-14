6c5f67c84c3cb528612a30f4744ceed387a7fa50
dagi420 <dagiovoxo@gmail.com>
2023-11-15 00:33:35 +0300

    0x07

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "main.h"
+
 /**
-*print_chessboard -main entry.
-*@a:char pointer
-*Description:function that prints the chessboard.
-* Return:void
-*/
+ * print_chessboard -main entry.
+ * @a:char pointer
+ * Description:function that prints the chessboard.
+ * Return:void
+ */
 void print_chessboard(char (*a)[8])
 {
 int x, y;
