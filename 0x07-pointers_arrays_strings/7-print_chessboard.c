238244e4777767f1ef3f038726ca5b1e792c145b
dagi420 <dagiovoxo@gmail.com>
2023-11-15 01:19:14 +0300

    0x07

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,29 +1,21 @@
-#include "holberton.h"
+#include <stdio.h>
+#include "main.h"
 
 /**
- * print_chessboard - prints the chess board
- *
- * @a: input pointer
- *
- * Return: return nothing
-*/
-
+ * print_chessboard -main entry.
+ * @a:char pointer
+ * Description:function that prints the chessboard.
+ * Return:void
+ */
 void print_chessboard(char (*a)[8])
 {
-	int i, j;
-
-	i = 0;
-	j = 0;
-	while (i < 64)
-	{
-		if (i % 8 == 0 && i != 0)
-		{
-			j = i;
-			_putchar('\n');
-		}
-
-		_putchar(a[i / 8][i - j]);
-		i++;
-	}
-	_putchar('\n');
+int x, y;
+for (x = 0; x < 8 ; x++)
+{
+for (y = 0; y < 8; y++)
+{
+putchar(a[x][y]);
+}
+putchar ('\n');
+}
 }
